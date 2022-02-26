fe29bb10f8ea8d6dfbbae90949b0c6c346beea3c
杨志强 <yangzhiqiang@winwin-inc.com>
2022-02-26 16:24:46 +0800

    feat(chore): add layouts,element plus,composeables examples

---
+++ pages/hi/[id].vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>Hi,</h3>
+    <div>{{ name }}!</div>
+
+    <template v-if="user.otherNames.length">
+      <div>
+        <span>Also as known as:</span>
+        <ul>
+          <li v-for="otherName in user.otherNames" :key="otherName">
+            <router-link :to="`/hi/${otherName}`" replace>
+              {{ otherName }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </template>
+
+    <Counter />
+
+    <div>
+      <NuxtLink to="/"> Back </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '~/stores/user';
+import Counter from '~~/components/client/Counter.vue';
+
+const route = useRoute();
+const user = useUserStore();
+const name = route.params.id;
+
+watchEffect(() => {
+  user.setNewName(route.params.id as string);
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <h3>Hi,</h3>
-  <div>{{ name }}!</div>
+  <div class="page-index">
+    <element-demo />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from '~/stores/user';
-
-const route = useRoute();
-const user = useUserStore();
-const name = route.params.id;
-
-watchEffect(() => {
-  user.setNewName(route.params.id as string);
-});
+import ElementDemo from '~~/components/client/ElementDemo.vue';
 </script>
+
+<style lang="scss" scoped>
+.page-index {
+  padding-top: 60px;
+  text-align: center;
+}
+</style>
