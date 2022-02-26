579c72c47bef1d22a9d7cc4c2beb6893dba5af68
heora <yueluo.yang@qq.com>
2022-02-26 17:08:42 +0800

    style: modify styles

--- pages/hi/[id].vue
+++ pages/hi/[id].vue
@@ -1,32 +1,41 @@
 <template>
-  <div>
-    <h3>Hi,</h3>
-    <div>{{ name }}!</div>
+  <div class="page-index">
+    <el-space direction="vertical" size="large">
+      <el-space>
+        <h3>Hi,</h3>
+        <div>{{ name }}!</div>
+      </el-space>
 
-    <template v-if="user.otherNames.length">
-      <div>
-        <span>Also as known as:</span>
-        <ul>
-          <li v-for="otherName in user.otherNames" :key="otherName">
-            <router-link :to="`/hi/${otherName}`" replace>
-              {{ otherName }}
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </template>
+      <template v-if="user.otherNames.length">
+        <div>
+          <span>Also as known as:</span>
+          <ul>
+            <li v-for="otherName in user.otherNames" :key="otherName">
+              <router-link :to="`/hi/${otherName}`" replace>
+                {{ otherName }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </template>
 
-    <Counter />
+      <Counter />
 
-    <div>
-      <NuxtLink to="/"> Back </NuxtLink>
-    </div>
+      <el-space>
+        <NuxtLink to="/"> Back </NuxtLink>
+      </el-space>
+    </el-space>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ElSpace } from 'element-plus/dist/index.full.js';
 import { useUserStore } from '~/stores/user';
 import Counter from '~~/components/client/Counter.vue';
+
+definePageMeta({
+  layout: false
+});
 
 const route = useRoute();
 const user = useUserStore();
@@ -36,3 +45,10 @@ watchEffect(() => {
   user.setNewName(route.params.id as string);
 });
 </script>
+
+<style lang="scss" scoped>
+.page-index {
+  padding-top: 60px;
+  text-align: center;
+}
+</style>
